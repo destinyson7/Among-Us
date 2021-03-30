d8b2120282d2fd01322baec8f21ce0c0b8ca2b19
destinyson7 <tanish.lad@research.iiit.ac.in>
2021-03-31 02:50:54 +0530

    player almost done

--- source/game.cpp
+++ source/game.cpp
@@ -1,9 +1,12 @@
 #include "game.h"
 #include "resource_manager.h"
 #include "maze.h"
+#include "player.h"
+// #include "texture.h"
 
 // Game-related State data
 Maze *Renderer;
+Player *PlayerRenderer;
 
 Game::Game(unsigned int width, unsigned int height)
 	: State(GAME_ACTIVE), Keys(), Width(width), Height(height)
@@ -13,23 +16,35 @@ Game::Game(unsigned int width, unsigned int height)
 Game::~Game()
 {
 	delete Renderer;
+	delete PlayerRenderer;
 }
 
 void Game::Init()
 {
 	// load shaders
-	ResourceManager::LoadShader("../source/shaders/shader.vert", "../source/shaders/shader.frag", nullptr, "sprite");
+	// ResourceManager::LoadShader("../source/shaders/shader.vert", "../source/shaders/shader.frag", nullptr, "sprite");
+	ResourceManager::LoadShader("../source/shaders/player.vert", "../source/shaders/player.frag", nullptr, "player");
+
 	// configure shaders
 	glm::mat4 projection = glm::ortho(0.0f, static_cast<float>(this->Width),
 									  static_cast<float>(this->Height), 0.0f, -1.0f, 1.0f);
+
 	// ResourceManager::GetShader("sprite").Use().SetInteger("image", 0);
-	ResourceManager::GetShader("sprite").Use().SetMatrix4("projection", projection);
+	// ResourceManager::GetShader("sprite").SetMatrix4("projection", projection);
+
+	ResourceManager::GetShader("player").Use().SetInteger("image", 0);
+	ResourceManager::GetShader("player").SetMatrix4("projection", projection);
+
 	// set render-specific controls
-	Shader spriteShader = ResourceManager::GetShader("sprite");
-	Renderer = new Maze(spriteShader);
+	// Shader spriteShader = ResourceManager::GetShader("sprite");
+	// Renderer = new Maze(spriteShader);
+
 	// Renderer = new SpriteRenderer(ResourceManager::GetShader("sprite"));
 	// load textures
-	// ResourceManager::LoadTexture("textures/awesomeface.png", true, "face");
+	ResourceManager::LoadTexture("../source/textures/among_us.png", true, "face");
+
+	Shader playerShader = ResourceManager::GetShader("player");
+	PlayerRenderer = new Player(playerShader);
 }
 
 void Game::Update(float dt)
@@ -42,5 +57,8 @@ void Game::ProcessInput(float dt)
 
 void Game::Render()
 {
-	Renderer->DrawMaze();
+	// Renderer->DrawMaze();
+
+	Texture2D playerTexture = ResourceManager::GetTexture("face");
+	PlayerRenderer->DrawPlayer(playerTexture, glm::vec2(200.0f, 200.0f), glm::vec2(300.0f, 400.0f), 45.0f, glm::vec3(0.0f, 1.0f, 0.0f));
 }
--- source/resource_manager.cpp
+++ source/resource_manager.cpp
@@ -4,10 +4,10 @@
 #include <sstream>
 #include <fstream>
 
-// #include "stb_image.h"
+#include "stb_image.h"
 
 // Instantiate static variables
-// std::map<std::string, Texture2D> ResourceManager::Textures;
+std::map<std::string, Texture2D> ResourceManager::Textures;
 std::map<std::string, Shader> ResourceManager::Shaders;
 
 Shader ResourceManager::LoadShader(const char *vShaderFile, const char *fShaderFile, const char *gShaderFile, std::string name)
@@ -21,16 +21,16 @@ Shader ResourceManager::GetShader(std::string name)
 	return Shaders[name];
 }
 
-// Texture2D ResourceManager::LoadTexture(const char *file, bool alpha, std::string name)
-// {
-// 	Textures[name] = loadTextureFromFile(file, alpha);
-// 	return Textures[name];
-// }
+Texture2D ResourceManager::LoadTexture(const char *file, bool alpha, std::string name)
+{
+	Textures[name] = loadTextureFromFile(file, alpha);
+	return Textures[name];
+}
 
-// Texture2D ResourceManager::GetTexture(std::string name)
-// {
-// 	return Textures[name];
-// }
+Texture2D ResourceManager::GetTexture(std::string name)
+{
+	return Textures[name];
+}
 
 void ResourceManager::Clear()
 {
@@ -38,8 +38,8 @@ void ResourceManager::Clear()
 	for (auto iter : Shaders)
 		glDeleteProgram(iter.second.ID);
 	// (properly) delete all textures
-	// for (auto iter : Textures)
-	// 	glDeleteTextures(1, &iter.second.ID);
+	for (auto iter : Textures)
+		glDeleteTextures(1, &iter.second.ID);
 }
 
 Shader ResourceManager::loadShaderFromFile(const char *vShaderFile, const char *fShaderFile, const char *gShaderFile)
@@ -86,21 +86,21 @@ Shader ResourceManager::loadShaderFromFile(const char *vShaderFile, const char *
 	return shader;
 }
 
-// Texture2D ResourceManager::loadTextureFromFile(const char *file, bool alpha)
-// {
-// 	// create texture object
-// 	Texture2D texture;
-// 	if (alpha)
-// 	{
-// 		texture.Internal_Format = GL_RGBA;
-// 		texture.Image_Format = GL_RGBA;
-// 	}
-// 	// load image
-// 	int width, height, nrChannels;
-// 	unsigned char *data = stbi_load(file, &width, &height, &nrChannels, 0);
-// 	// now generate texture
-// 	texture.Generate(width, height, data);
-// 	// and finally free image data
-// 	stbi_image_free(data);
-// 	return texture;
-// }
+Texture2D ResourceManager::loadTextureFromFile(const char *file, bool alpha)
+{
+	// create texture object
+	Texture2D texture;
+	if (alpha)
+	{
+		texture.Internal_Format = GL_RGBA;
+		texture.Image_Format = GL_RGBA;
+	}
+	// load image
+	int width, height, nrChannels;
+	unsigned char *data = stbi_load(file, &width, &height, &nrChannels, 0);
+	// now generate texture
+	texture.Generate(width, height, data);
+	// and finally free image data
+	stbi_image_free(data);
+	return texture;
+}
